0a3659f6700522b3effe625805871154cfab309e
j991132 <j991132@gmail.com>
2021-05-10 22:23:17 +0900

    0510 동적버튼 생성중

--- app/src/main/java/com/example/koreanclass/page.java
+++ app/src/main/java/com/example/koreanclass/page.java
@@ -1,14 +1,26 @@
 package com.example.koreanclass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class page extends AppCompatActivity {
+
+    private ConstraintLayout pageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page);
+
+        pageview = (ConstraintLayout)findViewById(R.id.pageview);
+        Button btn = new Button(this);
+        btn.setId(Integer.parseInt("1"));
+        btn.setText("버튼");
+        btn.setX(250);
+        btn.setY(250);
+        pageview.addView(btn);
     }
 }
